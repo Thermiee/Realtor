ed4e235e3f2b9d45229bac4e9d74d99216b0787a
Thermiee <navyblack16@yahoo.com>
2023-06-20 23:50:27 +0100

    fix linter error

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-export const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+const Nav = () => (
+  <div>Nav</div>
+);
+
+export default Nav;
